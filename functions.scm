4c4634caf3aec8265bb147f9611ea9cbb67f3cc3
Simon Buist <simon.buist@gmail.com>
2014-01-26 13:13:44 +0000

    Add totally boring multiinsertLR function

--- functions.scm
+++ functions.scm
@@ -1326,4 +1326,26 @@
     (a-friend (cons (quote and) newlat) 
 	      seen)))
 
+(define multiinsertLR
+  (lambda (new oldL oldR lat)
+    (cond
+      ((null? lat) (quote ()))
+      ((eq? (car lat) oldL)
+       (cons new
+	     (cons oldL
+		   (multiinsertLR new oldL oldR
+				  (cdr lat)))))
+      ((eq? (car lat) oldR)
+       (cons oldR
+	     (cons new
+		   (multiinsertLR new oldL oldR
+				  (cdr lat)))))
+      (else
+	(cons (car lat)
+	      (multiinsertLR new oldL oldR (cdr lat)))
+	)
+      )
+    )
+  )
+
 
