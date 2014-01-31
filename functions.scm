23e6549a8b8fe2362f2cfe5e5393984a0240f774
Simon Buist <simon.buist@gmail.com>
2014-01-31 16:34:34 +0000

    Add mind-pretzelising evens-only*&co featuring double-nested lambda collector for recurring on both the car and the cdr when we have a pair

--- functions.scm
+++ functions.scm
@@ -1400,3 +1400,38 @@
       )
     )
   )
+
+(define evens-only*&co
+  (lambda (l col)
+    (cond
+      ((null? l) (col (quote ()) 1 0))
+      ((atom? (car l))
+       (cond
+	 ((even? (car l))
+	  (evens-only*&co (cdr l)
+			  (lambda (newl p s)
+			    (col (cons (car l) newl) 
+				 (* (car l) p) s))))
+	 (else
+	   (evens-only*&co (cdr l)
+			   (lambda (newl p s)
+			     (col newl
+				  p (+ (car l) s)))))))
+      (else
+	(evens-only*&co (car l)
+			(lambda (al ap as)
+			  (evens-only*&co (cdr l)
+					  (lambda (dl dp ds)
+					    (col (cons al dl)
+						 (* ap dp)
+						 (+ as ds))))))
+	)
+      )
+    )
+  )
+
+(define the-last-friend
+  (lambda (newl product sum)
+    (cons sum
+	  (cons product
+		newl))))
