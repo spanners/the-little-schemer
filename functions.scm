b0f2ff9086f152c024853edfece587cc3cb8e7a6
Simon Buist <simon.buist@gmail.com>
2014-01-31 23:49:43 +0000

    Add a bunch of eval-like functions!

--- functions.scm
+++ functions.scm
@@ -1517,3 +1517,187 @@
     ((lambda (f) (f f))
      (lambda (f)
        (le (lambda (x) ((f f) x)))))))
+
+; 10. What Is the Value of All of This?
+
+(define entry1 (quote ((tuna beans toast butter) (fish pulses base spread))))
+(define entry2 (quote ((dog cat budgie ferret) (woof meow tweet silence))))
+
+(define new-entry build)
+
+(define lookup-in-entry
+  (lambda (name entry entry-f)
+    (lookup-in-entry-help name
+			  (first entry)
+			  (second entry)
+			  entry-f)))
+
+(define lookup-in-entry-help
+  (lambda (name names values entry-f)
+    (cond
+      ((null? names) (entry-f name))
+      ((eq? (car names) name) (car values))
+      (else
+	(lookup-in-entry-help name (cdr names) (cdr values) entry-f)
+	)
+      )
+    )
+  )
+
+(define table1 (cons entry1 (cons entry2 (quote ()))))
+
+(define extend-table cons)
+
+(define lookup-in-table
+  (lambda (name table table-f)
+    (cond
+      ((null? table) (table-f name))
+      (else
+	(lookup-in-entry name (car table) 
+			 (lambda (name)
+			   (lookup-in-table name
+					    (cdr table)
+					    table-f)))
+	)
+      )
+    )
+  )
+
+(define expression-to-action
+  (lambda (e)
+    (cond
+      ((atom? e) (atom-to-action e))
+      (else
+	(list-to-action e)
+	)
+      )
+    )
+  )
+
+(define atom-to-action
+  (lambda (e)
+    (cond
+      ((number? e) *const)
+      ((eq? e #t) *const)
+      ((eq? e #f) *const)
+      ((eq? e (quote cons)) *const)
+      ((eq? e (quote car)) *const)
+      ((eq? e (quote cdr)) *const)
+      ((eq? e (quote null?)) *const)
+      ((eq? e (quote eq?)) *const)
+      ((eq? e (quote atom?)) *const)
+      ((eq? e (quote zero?)) *const)
+      ((eq? e (quote add1)) *const)
+      ((eq? e (quote sub1)) *const)
+      ((eq? e (quote number?)) *const)
+      (else *identifier))))
+
+(define list-to-action
+  (lambda (e)
+    (cond
+      ((atom? (car e))
+       (cond
+	 ((eq? (car e) (quote quote))
+	  *quote)
+	 ((eq? (car e) (quote lambda))
+	  *lambda)
+	 ((eq? (car e) (quote cond))
+	  *cond)
+	 (else *application)))
+      (else 
+	*application
+	)
+      )
+    )
+  )
+
+(define value
+  (lambda (e)
+    (meaning e (quote ()))))
+
+(define meaning
+  (lambda (e table)
+    ((expression-to-action e) e table)))
+
+(define *const
+  (lambda (e table)
+    (cond
+      ((number? e) e)
+      ((eq? e #t) #t)
+      ((eq? e #f) #f)
+      (else 
+	(build (quote primitive) e)
+	)
+      )
+    )
+  )
+
+(define *identifier
+  (lambda (e table)
+    (lookup-in-table e table initial-table)))
+
+(define *quote
+  (lambda (e table)
+    (text-of e)))
+
+(define text-of second)
+
+(define initial-table
+  (lambda (name)
+    (car (quote ()))))
+
+(define *lambda
+  (lambda (e table)
+    (build (quote non-primitive)
+	   (cons table (cdr e)))))
+
+(define table-of first)
+
+(define formals-of second)
+
+(define body-of third)
+
+
+; WARNING! We violate the First Commandment here as we don't ask (null? lines), so one of the questions in every cond better be true!
+(define evcon
+  (lambda (lines table)
+    (cond
+      ((else? (question-of (car lines)))
+       (meaning (answer-of (car lines))
+		table))
+      ((meaning (question-of (car lines)) 
+		table)
+       (meaning (answer-of (car lines))
+		table))
+      (else 
+	(evcon (cdr lines) table)
+	)
+      )
+    )
+  )
+
+(define else?
+  (lambda (x)
+    (cond
+      ((atom? x) (eq? x (quote else)))
+      (else
+	#f
+	)
+      )
+    )
+  )
+
+(define question-of first)
+
+(define answer-of second)
+
+(define *cond
+  (lambda (e table)
+    (evcon (cond-lines-of e) table)))
+
+(define cond-lines-of cdr)
+
+(define e1 '(cond (coffee klatsch) (else party)))
+
+(define table2 '(((coffee) (#t)) (klatsch party) (5 (6))))
+
